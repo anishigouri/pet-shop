b6148d1b0b85af8477da7826b4df7451cecd6f4f
Allan Furlani Nishigouri <allanfurlani@gmail.com>
2026-06-18 09:54:35 -0300

    Add a new Component to render with props in the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const Component = ({ params }: Props) => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Component params={{ id: 1 }} />
     </div>
   );
 }
