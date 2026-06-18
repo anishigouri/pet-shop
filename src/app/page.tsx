aae82f2d34e31e9aa0ab7443f9455dbd9acde520
Allan Furlani Nishigouri <allanfurlani@gmail.com>
2026-06-18 09:57:18 -0300

    Remove unused Component from Home page and simplify structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-const Component = ({ params }: Props) => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-};
-
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Component params={{ id: 1 }} />
     </div>
   );
 }
